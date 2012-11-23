fcf9a36e7416c8ed3de1dc886cab4772b0fb2e7b
Andrew Heaford <ahphoshi@gmail.com>
2012-11-23 21:10:59 +0000

    Changed test case to be more demanding
    Elements are now inserted at various points throughout the list to
    ensure fair load

--- Program.cs
+++ Program.cs
@@ -7,45 +7,10 @@ namespace LinkedListTest{
         private static void Main(){
             var intList = new LinkedList<int>{22, 33, 44};
 
-            //LinkedList<string> stringList = new List<string>{"hi!", "boop!"}.ToLinkedList();
-            //Console.WriteLine(stringList);
-
-
-            intList.Append(1);
-            intList.Append(2);
-            intList.Append(3);
-
-            Console.WriteLine(intList.Contains(99));
-            Console.WriteLine(intList.Contains(22));
-
-            intList.Prepend(0);
-            intList.Prepend(-1);
-
-            intList.Insert(17, 3);
-
-            intList.Remove(2);
-            Console.WriteLine(intList);
-
-            intList.RemoveAt(1);
-            Console.WriteLine(intList);
-
-            Console.WriteLine(intList[0]);
-            Console.WriteLine(intList[1]);
-            Console.WriteLine(intList[2]);
-
-            foreach (var i in intList){
-                Console.WriteLine(i);
+            for (int i = 0; i < 100000; i++){
+                intList.Insert(i, intList.Length/((i%3) + 1));
             }
-
-            Console.WriteLine(string.Join(", ", from number in intList where number > 20 select number));
-
-            int j = 0;
-            while (j < 5000){
-                j++;
-                intList.Append(j);
-            }
-            Console.WriteLine(intList);
-
+            Console.WriteLine(intList.Length);
 
         }
     }
